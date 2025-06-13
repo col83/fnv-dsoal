8dc486ff0277863436c8ce3c774d174761b98b9a
dashi <Vyalko67@gmail.com>
2025-06-13 04:23:20 +0300

    system environment change
    using %SYSTEMROOT% instead of %SYSTEMDRIVE%\Windows
    
    same effect, but shorter

--- install.bat
+++ install.bat
@@ -2,8 +2,8 @@
 
 ".\OpenAL Installer.exe"
 
-copy /V /Y ".\dll\soft_oal.dll" "%SYSTEMDRIVE%\Windows\System32\"
-copy /V /Y ".\dll\soft_oal.dll" "%SYSTEMDRIVE%\Windows\SysWOW64\"
+copy /V /Y ".\dll\soft_oal.dll" "%SYSTEMROOT%\System32\"
+copy /V /Y ".\dll\soft_oal.dll" "%SYSTEMROOT%\SysWOW64\"
 
 call ".\regs.bat"
 
