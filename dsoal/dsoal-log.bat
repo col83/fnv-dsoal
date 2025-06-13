d30a7cb181e8d7e8d9b4625c5ba50e886832d2fc
dashi <Vyalko67@gmail.com>
2025-06-13 04:00:54 +0300

    refactor log "checker"

---
+++ dsoal/dsoal-log.bat
@@ -0,0 +1,28 @@
+:: set DSOAL_LOGLEVEL to 1 error, 2 warnings, 3 verbose (default)
+
+:: more info - https://github.com/kcat/openal-soft/issues/976
+
+
+::manually use library
+set ALROUTER_ACCEPT=soft_oal.dll
+
+::or use any instead default
+::set ALROUTER_REJECT=wrap_oal.dll
+
+
+set DSOAL_LOGLEVEL=1
+set DSOAL_LOGFILE=dsoal.log
+del /q .\dsoal.log
+
+
+:: run the game through FalloutNV.exe (it must be patched to use LAA (Large Address Aware)).
+
+:: more info:
+:: https://learn.microsoft.com/cpp/build/reference/largeaddressaware-handle-large-addresses?view=msvc-170
+
+:: https://www.nexusmods.com/newvegas/mods/62552?tab=description
+
+:: otherwise use nvse_loader.exe
+
+:: game launch
+FalloutNV.exe
