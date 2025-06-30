b364a296e4b45b98b18a1c945321694b93e83a66
dashi <Vyalko67@gmail.com>
2025-07-01 01:07:47 +0300

    Update check_info.bat

--- check_info.bat
+++ check_info.bat
@@ -1,29 +1,30 @@
 @echo off
 
-:: color section
+REM color section
 set COLOR_GREEN=[32m>nul
 set COLOR_YELLOW=[1;33m>nul
 set COLOR_RESET=[0m>nul
 
 
-:: manually use library
+REM manually use library
 set ALROUTER_ACCEPT=soft_oal.dll
 set ALROUTER_REJECT=wrap_oal.dll
 
-:: more info - https://github.com/kcat/openal-soft/issues/976
+REM more info - https://github.com/kcat/openal-soft/issues/976
 
 
 
 
 cls
 echo.
-".\openal-info.exe"
-echo.
+IF EXIST ".\openal-info.exe" (".\openal-info.exe" && goto log_file)
+exit 1
 
-:: log file
-IF EXIST "%CD%\openal-info.log" (del /q "%CD%\openal-info.log")
-".\openal-info.exe" > ".\openal-info.log"
-echo %COLOR_YELLOW% .log file created - "%CD%\openal-info.log" %COLOR_RESET%
+:log_file
+REM log file
+echo.
+".\openal-info.exe" > "%USERPROFILE%\Desktop\openal-info.log"
+echo %COLOR_YELLOW% .log file created - "%USERPROFILE%\Desktop\openal-info.log" %COLOR_RESET%
 
 echo.
 pause
