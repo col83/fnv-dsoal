01a1cee19fa7ef1d963b5566db9993269c78c976
dashi <Vyalko67@gmail.com>
2025-07-05 06:13:36 +0300

    refact

--- check-info.bat
+++ check-info.bat
@@ -7,7 +7,7 @@ set COLOR_RESET=[0m>nul
 
 
 REM manually use library
-set ALROUTER_ACCEPT=soft_oal.dll
+IF EXIST "%SYSTEMROOT%\System32\soft_oal.dll" (set ALROUTER_ACCEPT=soft_oal.dll)
 set ALROUTER_REJECT=wrap_oal.dll
 
 REM more info - https://github.com/kcat/openal-soft/issues/976
@@ -15,7 +15,11 @@ REM more info - https://github.com/kcat/openal-soft/issues/976
 
 cls
 echo.
-IF EXIST ".\openal-info.exe" (echo %COLOR_YELLOW% Current device info: %COLOR_RESET% & echo. & ".\openal-info.exe" && goto log_file)
+IF EXIST ".\openal-info.exe" (
+echo %COLOR_YELLOW% Current device info: %COLOR_RESET%
+echo.
+".\openal-info.exe" && goto log_file
+)
 exit 1
 
 :log_file
