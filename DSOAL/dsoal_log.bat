9937225f0a958d1f9b22e931b4968aef2206bba8
dashi <Vyalko67@gmail.com>
2025-07-09 13:46:00 +0300

    refactor log

--- DSOAL/dsoal_log.bat
+++ DSOAL/dsoal_log.bat
@@ -1,4 +1,7 @@
-REM set DSOAL_LOGLEVEL to 1 errors, 2 warnings (default), 3 verbose
+REM change game dir & exe if you need
+
+set GAMEDIR="%SYTEMDRIVE%\Games\Fallout New Vegas"
+set exe=%GAMEDIR%\nvse_loader.exe
 
 
 REM manually use library
@@ -8,8 +11,18 @@ set ALROUTER_REJECT=wrap_oal.dll
 REM more info - https://github.com/kcat/openal-soft/issues/976
 
 
-set DSOAL_LOGLEVEL=3
-set DSOAL_LOGFILE=dsoal.log
+REM set DSOAL_LOGLEVEL to 1 errors, 2 warnings (default), 3 verbose
+
+
+set LOGDIR=%USERPROFILE%\Documents
+
+set ALSOFT_LOGLEVEL=3
+set DSOAL_LOGLEVEL=2
+
+set ALSOFT_LOGFILE=%LOGDIR%\alsoft.log
+set DSOAL_LOGFILE=%LOGDIR%\dsoal.log
+
+IF EXIST ".\%ALSOFT_LOGFILE%" (del /q ".\%ALSOFT_LOGFILE%")
 IF EXIST ".\%DSOAL_LOGFILE%" (del /q ".\%DSOAL_LOGFILE%")
 
 
@@ -23,4 +36,4 @@ REM https://www.nexusmods.com/newvegas/mods/62552?tab=description
 REM otherwise use nvse_loader.exe
 
 REM game launch
-FalloutNV.exe
+%exe%
